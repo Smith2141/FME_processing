173abb02f61e6ffdaa1c3ef87c20e2b1f059ddc9
Smith <berez_max@mail.ru>
2019-12-05 22:59:19 +0300

    starting convert

--- script1.py
+++ script1.py
@@ -1,7 +1,15 @@
 import os
 import time
 
-os.system(
-    r'fme.exe 2_mapinfo2mapinfo.fmw --SourceData Lake_27_3.TAB --DestinationData C:\test_fme\RESULT')
+fme_file = r"C:\Users\1\FME_processing\2_mapinfo2mapinfo.fmw"
+source_dir = r"C:\LAYER_T"
+source_file = r"C:\LAYER_T\51_01_0000000.TAB"
+dest_dir = r"C:\LAYER_T\RES"
 
-time.sleep(5)
+# for root, dirs, files in os.walk(source_dir):
+#     for f in files:
+#         if f.endswith('.TAB'):
+#             print(f)
+#             os.system(f'fme.exe {fme_file} --SourceData {f} --DestinationData {dest_dir}')
+
+os.system(f'fme.exe {fme_file} --SourceData {source_file} --DestinationData {dest_dir} --NAME {source_file}')
