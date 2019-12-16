99cd20b90b8aa140d676ba73f8c0a9386d40d72d
Smith <berez_max@mail.ru>
2019-12-16 23:08:55 +0300

    complete add assertion in tab class

--- Layers/base_layer.py
+++ Layers/base_layer.py
@@ -1,4 +1,4 @@
 class BaseLayer:
-    def __init__(self, key_layer_name, layer_source_path):
-        self.layer_source_path = layer_source_path
-        self.layer_name = key_layer_name
+    def __init__(self, key_layer_name, layer_file_path):
+        self.key_layer_name = key_layer_name
+        self.layer_file_path = layer_file_path
--- Layers/only_tab_layer.py
+++ Layers/only_tab_layer.py
@@ -1,20 +1,23 @@
+import os
 import shutil
 from .base_layer import BaseLayer
+from Layers.locators import LayerExtension
 
 
-class ConvertibleTabLayer(BaseLayer, layer_final_path=None):
-    def copy_tab_with_three_files(self, tab_path, file_extension_iter, destination_path):
-        name_of_the_four_files = str(tab_path).rpartition('.')[0]
+class ConvertibleTabLayer(BaseLayer):
+    def copy_tab_with_three_files(self, destination_path):
+        name_of_the_four_files = str(self.layer_file_path).rpartition('.')[0]
         counter_copied_files = 0
-        for e in file_extension_iter:
-            required_file_upper_case = f'{name_of_the_four_files}.{e}'
-            required_file_lower_case = f'{name_of_the_four_files}.{e.lower()}'
+        for ext in LayerExtension.tab_layer_file_types:
+            required_file_upper_case = f'{name_of_the_four_files}.{ext}'
+            required_file_lower_case = f'{name_of_the_four_files}.{ext.lower()}'
             if os.path.exists(required_file_upper_case):
                 shutil.copy(required_file_upper_case, destination_path)
                 counter_copied_files += 1
             else:
                 shutil.copy(required_file_lower_case, destination_path)
                 counter_copied_files += 1
+        assert counter_copied_files == 4
         print(f'Всего скопировано {counter_copied_files} файлов')
 
     def from_tab_make_pls_tab(self):
