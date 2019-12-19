deb29352bba044a59941e09eb307f0a760b206d5
Smith <berez_max@mail.ru>
2019-12-19 23:14:32 +0300

    pl sch method added wn

--- Layers/common_action.py
+++ Layers/common_action.py
@@ -1,6 +1,8 @@
 import os
 import re
+import fmeobjects
 from Layers.locators import PatternsDpt
+from Layers.locators import WorkspacesPath
 
 
 def create_primary_search_dictionary(work_path, pattern=PatternsDpt):
@@ -19,3 +21,34 @@ def create_primary_search_dictionary(work_path, pattern=PatternsDpt):
                     if re.search(patt, file):
                         prim_search_dict[key_name].append(os.path.join(root, file))
     return prim_search_dict
+
+
+def tab_to_pl_sch_tab(prim_search_dict, target_dir):
+    # Full path to Workspace, example comes from the FME 2014 Training Full Dataset
+    workspace = WorkspacesPath.TAB_TO_PL_SCH_TAB
+    # Set workspace parameter s by creating a dictionary of name value pairs
+    parameters = dict()
+    parameters['DestDataset_MAPINFO'] = target_dir
+    parameters['FEATURE_TYPES'] = ""
+
+    counter = 0
+    for layer_name, paths in prim_search_dict.items():
+        for path in paths:
+            parameters['SourceDataset_MAPINFO'] = path
+            try:
+                # initiate FMEWorkspaceRunner Class
+                runner = fmeobjects.FMEWorkspaceRunner()
+                # Run Workspace with parameters set in above dictionary
+                runner.runWithParameters(workspace, parameters)
+                counter += 1
+            except fmeobjects.FMEException as ex:
+                # Print out FME Exception if worskspace failed
+                print(ex.message)
+            else:
+                # Tell user the workspace ran
+                print('The Workspace: ' + workspace)
+                print('...ran successfully')
+
+            # get rid of FMEWorkspace runner so we don't leave an FME process running
+            runner = None
+    return f'Преобразовано в план-схему метры {counter} файлов .tab'
--- script2.py
+++ script2.py
@@ -1,15 +1,17 @@
 import os
 from Layers.convertible_tab_layer import ConvertibleTabLayer
 from Layers.common_action import create_primary_search_dictionary
+from Layers.common_action import tab_to_pl_sch_tab
 
 # path_the_files_DPT = input('Укажите папку ДПТ: ')
 # path_electronic_version_of_DPT = input('Где будем создавать электронку?: ')
 path_the_files_DPT = os.path.join(os.getcwd(), r'test_fme')
 path_electronic_version_of_DPT = os.path.join(os.getcwd(), r'test_electronic_version')
+path_planned_files = os.path.join(os.getcwd(), r'test_electronic_version\planned_files')
 
 print('-' * 25, '\n', 'В указанной папке найдены слои:')
-primary_search_dictionary = create_primary_search_dictionary(path_the_files_DPT)
-for layer_name, paths in primary_search_dictionary.items():
+work_files_dictionary = create_primary_search_dictionary(path_the_files_DPT)
+for layer_name, paths in work_files_dictionary.items():
     print(f'{layer_name} {len(paths)} шт.: ')
     i = 1
     for path in paths:
@@ -20,9 +22,13 @@ if not os.path.isdir(path_electronic_version_of_DPT):
     os.mkdir(path_electronic_version_of_DPT)
 
 counter = 0
-for layer_name, paths in primary_search_dictionary.items():
+for layer_name, paths in work_files_dictionary.items():
     for p in paths:
         copying_layer = ConvertibleTabLayer(p)
         copying_layer.copy_tab_layer(path_electronic_version_of_DPT)
         counter += 1
 print(f'Из рабочей директории скопировано {counter} слоёв .tab')
+
+copied_files_dictionary = create_primary_search_dictionary(path_electronic_version_of_DPT)
+number_planned_tab_files = tab_to_pl_sch_tab(copied_files_dictionary, path_planned_files)
+print(number_planned_tab_files)
